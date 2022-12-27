9b1f50572191d7fb1ea8e265ecb67043347c940f
Pedro Elias <106087744+Elias-Pdr@users.noreply.github.com>
2022-12-26 21:40:16 -0300

    up

--- formatInput.js
+++ formatInput.js
@@ -22,3 +22,31 @@ function mCelular(celular){
     celular=celular.replace(/(\d{4})(\d{1,4})$/,"$1-$2")
     return celular
 }
+
+let contagem = 0
+function mValor(valor){
+    contagem++;
+    console.log(contagem);
+
+    if (contagem == 1) {
+        valor= "0,0" + valor
+    }
+    else if (contagem == 2) {
+        valor = valor.replace("0,0", "")
+        valor = "0," + valor
+    }
+    else if (contagem == 3) {
+        valor = valor.replace("0,", "")
+        valor = valor.replace(/(\d{1})(\d)/,"$1,$2")
+    }
+    else if (contagem == 4) {
+        valor = valor.replace(",", "")
+        valor = valor.replace(/(\d{2})(\d)/,"$1,$2")
+    }
+    else if (contagem == 5) {
+        valor = valor.replace(",", "")
+        valor = valor.replace(/(\d{3})(\d)/,"$1,$2")
+    }
+
+    return valor
+}
